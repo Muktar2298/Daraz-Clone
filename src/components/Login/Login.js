4123188981250fd9190ab26d1b4c3984d04014bd
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-11 02:49:59 +0600

    Google Auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useState } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
+import firebaseConfig from "./firebase.config";
+import {} from "firebase/auth";
+const app = initializeApp(firebaseConfig);
 
 const Login = () => {
-    return (
+  const [user, setUser] = useState({
+    isLoggedIn: false,
+    name: "",
+    email: "",
+    photoURL: "",
+  });
+  // --Google Provider --//
+  const provider = new GoogleAuthProvider();
+  // --Google Sign In --//
+  const googleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        const { displayName, email, photoURL } = user;
+        const userInfo = {
+          isLoggedIn: true,
+          name: displayName,
+          email: email,
+          photoURL: photoURL,
+        };
+        setUser(userInfo);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
+  //   --Google SignOut --//
+  const googleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then((res) => {
+        const userInfo = {
+          isLoggedIn: false,
+          name: "",
+          email: "",
+          photoURL: "",
+        };
+        setUser(userInfo);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div style={{ marginTop: "5%" }}>
+      {user.isLoggedIn && (
         <div>
-            <h1>This is LoginF</h1>
+          <img
+            src={user.photoURL}
+            alt="avatar"
+            style={{ width: "200px", height: "200px", borderRadius: "50%" }}
+          />
+          <p>{user.name}</p>
+          <p>{user.email}</p>
         </div>
-    );
+      )}
+      {/* --GoogleSign-- */}
+      {user.isLoggedIn ? (
+        <button type="button" onClick={googleSignOut}>
+          Google Sign Out
+        </button>
+      ) : (
+        <button type="button" onClick={googleSignIn}>
+          Google Sign In
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYPvywYTwcVuy_QWTEnBtZIyp3YxzhvB8",
+    authDomain: "daraz-online.firebaseapp.com",
+    projectId: "daraz-online",
+    storageBucket: "daraz-online.appspot.com",
+    messagingSenderId: "953753020906",
+    appId: "1:953753020906:web:8e6598a9a663a5d40b6a93"
+  };
+  
+  export default firebaseConfig;
